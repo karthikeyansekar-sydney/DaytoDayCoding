260cd005a5a5214f848077c366acec74a762c42d
karthikeyansekar <karthikeyansep27@gmail.com>
2020-05-03 23:28:22 +1000

    creating function  component

--- React/project-one/src/index.js
+++ React/project-one/src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// class Message extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//       <h1 style={{color: this.props.color}}>
-//       {this.props.msg}
-//       </h1>
-//       <p> I will be back in {this.props.minutes} minutes</p>
-//       </div>
-//     )
-//   }
-// }
+class Message extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1 style={{color: this.props.color}}>
+      {this.props.msg}
+      </h1>
+      <p> I will be back in {this.props.minutes} minutes</p>
+      </div>
+    )
+  }
+}
 
 let cricScore = {
   total: 260,
   wicket: 8,
-  overs: 45,
+  overs: 46,
   target: 320
 }
 
@@ -41,6 +41,25 @@ return (
 )
   }
 }
+
+const CricScoreCount = (props) => {
+  return (
+    <section>
+    <div>
+  <p> Total Score: {props.total} </p>
+    </div>
+    <div>
+  <p> Wickets Taken: {props.wicket} </p>
+    </div>
+    <div>
+  <p> Overs Bowled: {props.overs} </p>
+    </div>
+    <div>
+  <p> Target Score: {props.target} </p>
+    </div>
+    </section>
+  )
+}
 ReactDOM.render(
   // <Message msg="hello" color="green" minutes={4}/>, document.getElementById('root')
-<CricScoreCounter total={cricScore.total} wicket={cricScore.wicket} overs={cricScore.overs} target={cricScore.target}/>, document.getElementById('root'))
+<CricScoreCount total={cricScore.total} wicket={cricScore.wicket} overs={cricScore.overs} target={cricScore.target}/>, document.getElementById('root'))
