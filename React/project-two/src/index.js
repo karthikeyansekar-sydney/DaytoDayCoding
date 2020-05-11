a1ebc34635dc4caf4731ce04068451af2e90985b
karthikeyansekar <karthikeyansep27@gmail.com>
2020-05-11 22:48:13 +1000

    using proptypes

--- React/project-two/src/index.js
+++ React/project-two/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
+import PropTypes from 'prop-types';
 
-let bookList = [
+let bookList = {
   {"author": "Roxane gay", "pages": 300},
   {"title": "The sun also rises", "author": "Ernest Hemingway", "pages": 200},
   {"title": "White teeth", "author": "Zadie smith", "pages": 400},
   {"title": "cat's cradle", "author": "kyurt vonnegut", "pages": 320}
-  ]
+}
 
 class Library extends React.Component {
 
@@ -75,6 +76,16 @@ componentDidUpdate() {
   }
 }
 
+Library.propTypes = {
+  books: PropTypes.array
+}
+
+Book.propTypes = {
+  title: PropTypes.string,
+  author: PropTypes.string,
+  pages: PropTypes.number,
+  freeBookmark: PropTypes.bool
+}
 //state as props
 const Book = ({title="No Title Provided", author="No Author", pages=0, freeBookmark}) => {
   return (
