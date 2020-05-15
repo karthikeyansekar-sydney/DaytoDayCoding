b4d53e26216df4d24d3efc5bd60aaf0a19d03ac7
karthikeyansekar <karthikeyansep27@gmail.com>
2020-05-15 18:11:14 +1000

    Rendering arrays

---
+++ React/project-two/src/Arrayrendering.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Array = () => {
+return [
+  <li key="1">Hello</li>,
+  <li key="2">how </li>,
+  <li key="3">are </li>,
+  <li key="4">you </li>
+  ]
+}
--- React/project-two/src/index.js
+++ React/project-two/src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Library from './Library';
+import { Array } from './Arrayrendering';
 
 
-let bookList = [
-  {"author": "Roxane gay", "pages": 300},
-  {"title": "The sun also rises", "author": "Ernest Hemingway", "pages": 200},
-  {"title": "White teeth", "author": "Zadie smith", "pages": 400},
-  {"title": "cat's cradle", "author": "kyurt vonnegut", "pages": 320}
-]
 
-
-render(<Library books={bookList}/>, document.getElementById('root'));
+render(
+  <Array/>,
+  document.getElementById('root')
+);
