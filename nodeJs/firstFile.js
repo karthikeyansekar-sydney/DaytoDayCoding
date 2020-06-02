a91f890a70c9c64987e9354957fa65a966ddb527
karthikeyansekar <karthikeyansep27@gmail.com>
2020-06-02 16:05:45 +1000

    The require function

--- nodeJs/firstFile.js
+++ nodeJs/firstFile.js
@@ -1,17 +1,11 @@
-//console.log("Hello World");
-//const path = require("path");
-//console.log(`${path.basename(__filename)}`);
+let hello = "Hello World From node js";
+console.log(hello);
 
-//console.log(process.pid);
-//console.log(process.versions.node);
+console.log(__dirname);
+console.log(__filename);
+
+console.log("Hello World");
+const path = require("path"); //here we are loading external module
+console.log(`The file name is ${path.basename(__filename)}`);
 
 
-const grab = flag => {
-    let indexAfterFlag = process.argv.indexOf(flag) + 1;
-    return process.argv[indexAfterFlag];
-}
-
-const greeting = grab("--greeting");
-const user = grab("--user");
-
-console.log(` ${greeting} ${user}`);
