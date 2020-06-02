1b382efc30c83785fa831ce8f7776f4e08421d5f
karthikeyansekar <karthikeyansep27@gmail.com>
2020-06-02 16:07:08 +1000

    Argument variables with process.argv

--- nodeJs/firstFile.js
+++ nodeJs/firstFile.js
@@ -8,4 +8,15 @@ console.log("Hello World");
 const path = require("path"); //here we are loading external module
 console.log(`The file name is ${path.basename(__filename)}`);
 
+console.log(process.pid);
+console.log(process.versions.node);
 
+const grab = flag => {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
+}
+
+const greeting = grab("--greeting");
+const user = grab("--user");
+
+console.log(` ${greeting} ${user}`);
