06073d04e94a317e643543b1d4f215fb67974350
karthikeyansekar <karthikeyansep27@gmail.com>
2020-06-09 21:39:04 +1000

    simple demo for v8  module

--- nodeJs/core.js
+++ nodeJs/core.js
@@ -1,7 +1,7 @@
-const path = require("path");
-const util = require("util");
+const path = require("path"); // loading path module
+const util = require("util"); // loading util module
 const v8 = require("v8");
 
-util.log(path.basename(__filename));
-
+util.log(path.basename(__filename)); // in utility module we can print date and timestamp as well
 util.log(" ^ The name of the current file");
+util.log(v8.getHeapStatistics());
