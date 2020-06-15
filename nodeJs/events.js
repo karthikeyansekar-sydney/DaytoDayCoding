531139348dddf98babe303328f542118397d61e3
karthikeyansekar <karthikeyansep27@gmail.com>
2020-06-15 19:52:55 +1000

    Custom events with the EventEmitter

---
+++ nodeJs/events.js
@@ -0,0 +1,11 @@
+const events = require("events");
+const emitter = new events.EventEmitter();
+
+emitter.on("customEvent", (message, user) => {
+    console.log(`${user}: ${message}`);
+    });
+
+    emitter.emit("customEvent", "Hello World", "Computer");
+    emitter.emit("customEvent", "That's pretty cool huh?", "Karthik");
+    
+
