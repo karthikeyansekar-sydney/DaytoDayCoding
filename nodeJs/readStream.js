2cfb00a7ed93b7f213186374f8f2ef2bae9f3179
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-06-30 23:02:55 +1000

    readable file streams using fs module

--- nodeJs/readStream.js
+++ nodeJs/readStream.js
@@ -1,5 +1,7 @@
+const fs = require("fs");
+const readStream = fs.createReadStream("./assets/notes.md","UTF-8");
 console.log("type something...");
-process.stdin.on("data", data => {
+readStream.on("data", data => {
   console.log(`I read ${data.length - 1} characters of text`);
   process.exit();
 });
