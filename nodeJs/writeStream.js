95c283a0815e6d50d8a81db661004afb53b7b27d
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-07-01 22:58:36 +1000

    writing file  using readstream module

--- nodeJs/writeStream.js
+++ nodeJs/writeStream.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-const writeStream = fs.createWriteStream("./assets/myFile.txt","UTF-8");
-
-process.stdin.on("data", data => {
+const writeStream = fs.createWriteStream("./assets/myCopiedFile.txt","UTF-8");
+const readStream = fs.createReadStream("./assets/myFile.txt","UTF-8");
+readStream.on("data", data => {
     writeStream.write(data);
 });
