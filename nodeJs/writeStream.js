f475b658a516b35b162e8cbeec0ff247dc7b5235
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-07-02 22:16:20 +1000

    creating child process with exec

--- nodeJs/writeStream.js
+++ nodeJs/writeStream.js
@@ -2,6 +2,5 @@ const fs = require("fs");
 
 const writeStream = fs.createWriteStream("./assets/myCopiedFile.txt","UTF-8");
 const readStream = fs.createReadStream("./assets/myFile.txt","UTF-8");
-readStream.on("data", data => {
-    writeStream.write(data);
-});
+
+process.stdin.pipe(writeStream);
