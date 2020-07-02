5a1d8542a6d26418d050a33f5f36cadf84d48491
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-07-02 23:02:06 +1000

    child process with spawn along with exit function

--- nodeJs/spawn.js
+++ nodeJs/spawn.js
@@ -2,12 +2,14 @@ const cp = require("child_process");
 
 const questionApp = cp.spawn("node", ["questions.js"]);
 
+questionApp.stdin.write("karthikeyan \n");
+questionApp.stdin.write("sydney \n");
+questionApp.stdin.write("drive the future with technology \n");
+
 questionApp.stdout.on("data", data => {
-    console.log(`from the question app: ${data}`);
-    
-})
+  console.log(`from the question app: ${data}`);
+});
 
 questionApp.on("close", () => {
-    console.log(`questionApp process exited`);
-    
+  console.log(`questionApp process exited`);
 });
