7bd19e8db2468e3ec23f5a19911f15985b07b820
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-07-04 23:16:10 +1000

    writestream with pipe function

--- nodeJs/writeStream.js
+++ nodeJs/writeStream.js
@@ -3,4 +3,4 @@ const fs = require("fs");
 const writeStream = fs.createWriteStream("./assets/myCopiedFile.txt","UTF-8");
 const readStream = fs.createReadStream("./assets/myFile.txt","UTF-8");
 
-process.stdin.pipe(writeStream);
+readStream.pipe(writeStream);
